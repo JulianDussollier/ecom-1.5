2944b9611f131fe147f19830026dd6006b5322ff
Julian <dussollier.julian@gmail.com>
2023-11-07 14:46:10 -0500

    signupp form and result page

---
+++ pages/signup.php
@@ -0,0 +1,13 @@
+<h2>S'enregistrer</h2>
+<a href="../">Accueil</a>
+<!-- Chaque formulaire a sa page de résultats -->
+<!-- Todo : changer les types pour validation front -->
+<form method="post" action="../results/signupResult.php">
+<label for="user_name">Nom d'utilisateur</label>
+<input id="user_name" type="text" name="user_name" value="">
+<label for="email">Courriel</label>
+<input id="email" type="text" name="email" value="">
+<label for="pwd">Mot de passe</label>
+<input id="pwd" type="text" name="pwd" value="">
+<button type="submit">Soumettre mon enregistrement</button>
+</form>
---
+++ results/signupResult.php
@@ -0,0 +1,8 @@
+<?php
+
+// Todo : valider les données de mon form 
+// si les données ne sont pas bonne : renvoyer vers le form d'enregistrement (redirect auto )
+// attention on veut récupérer les données rentrées précédement : $_SESSION
+
+// Todo : traiter les données de mon form
+// envoyer les données dans la DB
