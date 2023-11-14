048bdbc97c5d9a6961fe69b10746e8ba2bcc17f8
Julian <dussollier.julian@gmail.com>
2023-11-14 13:15:01 -0500

    validation  and error management in Session

--- functions/validation.php
+++ functions/validation.php
@@ -30,3 +30,41 @@ function usernameIsValid(string $username): array
     }
     return $result;
 }
+
+function emailIsValid($email)
+{
+
+    $email_validation_regex = "/^[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/";
+    if (!preg_match($email_validation_regex, $email)) {
+        return [
+            'isValid' => false,
+            'msg' => "Format d'email invalid",
+        ];
+    }
+    return [
+        'isValid' => true,
+        'msg' => '',
+    ];
+}
+
+function pwdLenghtValidation($pwd)
+{
+    //minimum 8 max 16
+    $length = strlen($pwd);
+
+    if ($length < 8) {
+        return [
+            'isValid' => false,
+            'msg' => 'Votre mot de passe est trop court. Doit être supérieur a 8 caractères'
+        ];
+    } elseif ($length > 16) {
+        return [
+            'isValid' => false,
+            'msg' => 'Votre mot de passe est trop long. Doit être inférieur a 16 caractères'
+        ];
+    }
+    return [
+        'isValid' => true,
+        'msg' => ''
+    ];
+}
